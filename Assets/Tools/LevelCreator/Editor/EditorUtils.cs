0166c4571f031470c1961aa168898f6cf09d5ad3
wswiders <wojtek.swiderski8@gmail.com>
2019-06-22 14:47:02 +0200

    gitignore update, unity in new version open, started working on level creation tool

---
+++ Assets/Tools/LevelCreator/Editor/EditorUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace RunAndJump.LevelCreator
+{
+    public static class EditorUtils
+    {
+        public static void NewLevel()
+        {
+            _newScene();
+            GameObject levelGo = new GameObject("Level");
+            levelGo.transform.position = Vector3.zero;
+            levelGo.AddComponent<Level>();
+        }
+
+        private static void _newScene()
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+        }
+    }
+}
+
---
+++ Assets/Tools/LevelCreator/Editor/MenuItems.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RunAndJump.LevelCreator
+{
+    public static class MenuItems
+    {
+        [MenuItem("Tools/Level Creator/Create New Scene")]
+        public static void CreateScene()
+        {
+            EditorUtils.NewLevel();
+        }
+    }
+}
